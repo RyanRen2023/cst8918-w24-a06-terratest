16547c6881810cee5d565facfd9661f13f1f5897
RyanRen2023 <ren00055@algonquinlive.com>
2025-06-12 18:44:40 -0400

    modify variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 # Define config variables
 variable "labelPrefix" {
   type        = string
+  default     = "ren00055"
   description = "Your college username. This will form the beginning of various resource names."
 }
 
